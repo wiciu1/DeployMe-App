508e1ba89dab02e9bfcd984de0c63f71523c94ab
= <wtezycki1@gmail.com>
2025-08-01 20:23:47 +0200

    First commit. Structure of Spring Boot application

---
+++ backend/src/main/java/dev/deployme/DeployMe/DeployMeApplication.java
@@ -0,0 +1,13 @@
+package dev.deployme.DeployMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployMeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/deployme/DeployMe/DeployMeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deployme.DeployMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
