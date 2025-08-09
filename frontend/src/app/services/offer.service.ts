166580603c356e052a8a0e7095ab66e602eb46ec
= <wtezycki1@gmail.com>
2025-08-09 10:35:25 +0200

    Frontend filtering, without UI

--- frontend/src/app/services/offer.service.ts
+++ frontend/src/app/services/offer.service.ts
@@ -3,6 +3,12 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Offer } from '../models/offer';
 
+interface OfferFilters {
+  location?: string[];
+  experience?: string[];
+  skill?: string[];
+}
+
 interface PaginatedResponse<T> {
   content: T[];
   totalElements: number;
@@ -22,8 +28,7 @@ export class OfferService {
     
     private http = inject(HttpClient);
 
-
-    // Sends GET request to backend ( gets offers existing in DB )
+        // Sends GET request to backend ( gets offers existing in DB )
     getOffers(page: number = 0, perPage: number = 15) : Observable<PaginatedResponse<Offer>> {
 
       const params = new HttpParams()
@@ -31,6 +36,40 @@ export class OfferService {
         .set('perPage', perPage.toString())
 
       return this.http.get<PaginatedResponse<Offer>>(this.apiUrl, { params });
+    }
+
+
+    // Sends GET request to backend with filters
+    getFilteredOffers(
+      filters: OfferFilters,
+      page: number = 0,
+      perPage: number = 15
+    ): Observable<PaginatedResponse<Offer>> {
+
+        let params = new HttpParams()
+          .set('page', page.toString())
+          .set('perPage', perPage.toString());
+
+        if (filters.location) {
+          filters.location.forEach(loc => {
+            params = params.append('location', loc);
+          }) 
+        }
+
+        if (filters.experience) {
+          filters.experience.forEach(exp => {
+            params = params.append('experience', exp);
+          }) 
+        }
+
+        if (filters.skill) {
+          filters.skill.forEach(skill => {
+            params = params.append('skill', skill);
+          }) 
+        }
+
+
+        return this.http.get<PaginatedResponse<Offer>>(`${this.apiUrl}/filter`, { params });
     }
 
     // Sends POST request to backend ( scrape data )
